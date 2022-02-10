02b49818334fa85afd1d20b824a7e771a47ea362
Don Odom <divdonis@gmail.com>
2022-02-09 20:15:42 -0800

    debugging action cable

--- app/channels/channels_channel.rb
+++ app/channels/channels_channel.rb
@@ -1,14 +1,16 @@
 class ChannelsChannel < ApplicationCable::Channel
 
   def subscribed
-    @channel = Channel.find(params[:room])
-    stream_for @channel
+    stop_all_streams
+    channel = Channel.find(params[:room])
+    stream_for channel
   end
 
   def receive(data)
-    ChannelsChannel.broadcast_to(@channel,{ 
-      message: @channel.messages.last
-    })
+    ChannelsChannel.broadcast_to(channel,{ 
+        message: channel.messages.last
+      }
+  )
   end
 
   def unsubscribed
