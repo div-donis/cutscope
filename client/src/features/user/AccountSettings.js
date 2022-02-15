40564d601a2711a4e4d61d734272ef91a4cc2fd4
Don Odom <divdonis@gmail.com>
2022-02-15 10:48:14 -0800

    AWS S3 functional for user update image

--- client/src/features/user/AccountSettings.js
+++ client/src/features/user/AccountSettings.js
@@ -4,8 +4,28 @@ import { useNavigate } from "react-router-dom";
 import CryptoJS from 'crypto-js'
 import './User.css'
 
-
-
+const md5FromFile = (file) => {
+    // https://stackoverflow.com/questions/34495796
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+    
+      reader.onload = (fileEvent) => {
+        let binary = CryptoJS.lib.WordArray.create(fileEvent.target.result)
+        const md5 = CryptoJS.MD5(binary)
+        resolve(md5)
+      }
+      reader.onerror = () => {
+        reject('oops, something went wrong with the file reader.')
+      }
+      reader.readAsArrayBuffer(file)
+    })
+  }
+  
+  export const fileChecksum = async(file) => {
+    const md5 = await md5FromFile(file)
+    const checksum = md5.toString(CryptoJS.enc.Base64)
+    return checksum
+  }
 
 const AccountSettings = () => {
 
@@ -17,155 +37,66 @@ const AccountSettings = () => {
 
     const dispatch = useDispatch()
 
-    // Taken from https://elliott-king.github.io/2020/09/s3-heroku-rails/
-    const md5FromFile = (file) => {
-
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader()
-            
-            reader.onload = (fileEvent) => {
-                let binary = CryptoJS.lib.WordArray.create(fileEvent.target.result)
-                const md5 = CryptoJS.MD5(binary)
-                resolve(md5)
-            }
-            reader.onerror = () => {
-                reject('oops, something went wrong with the file reader.')
-            }
-
-            reader.readAsArrayBuffer(file)
-        })
-    }
-    
-    const fileChecksum = async(file) => {
-        const md5 = await md5FromFile(file)
-        const checksum = md5.toString(CryptoJS.enc.Base64)
-        return checksum
-    }
-
-   {/* const createPresignedUrl = async(file, byte_size, checksum) => {
+    // Sourced from https://elliott-king.github.io/2020/09/s3-heroku-rails/
+    const createPresignedUrl = async(file, byte_size, checksum) => {
         let options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                file: {
-                filename: file.name,
-                byte_size: byte_size,
-                checksum: checksum,
-                content_type: 'image/png',
-                metadata: {
-                    'message': 'image for parsing'
-                }
-                }
-            })
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            file: {
+              filename: file.name,
+              byte_size: byte_size,
+              checksum: checksum,
+              content_type: 'image/png',
+              metadata: {
+                'message': 'profile image'
+              }
+            }
+          })
         }
-
-        const presignedUrlEndpoint  = '/presigned_url'
-
-        let res = await fetch(presignedUrlEndpoint, options)
+        let res = await fetch('/presigned_url', options)
         if (res.status !== 200) return res
         return await res.json()
-    }  */}
-
-    {/*const updateImage = async(png) => {  
-        // To upload pdf file to S3, we need to do three steps:
-        // 1) request a pre-signed PUT request (for S3) from the backend
-    
-        const checksum = await fileChecksum(png)
-        const presignedFileParams = await createPresignedUrl(png, png.size, checksum)
-        
-        // 2) send file to said PUT request (to S3)
-        const s3PatchOptions = {
-        method: 'PATCH',
-        headers: presignedFileParams.direct_upload.headers,
-        body: png,
-        }
-        let awsRes = await fetch(presignedFileParams.direct_upload.url, s3PatchOptions)
-        if (awsRes.status !== 200) return awsRes
-    
-        // 3) confirm & create user with backend
-        let usersPatchOptions = {
-        method: 'PATCH',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            png: presignedFileParams.blob_signed_id,
-        })
-        }
-
-        const userEndpoint  = `/api/users/${user.id}`
-
-        let res = await fetch(userEndpoint, usersPatchOptions)
-        if (res.status !== 200) return res 
-        return await res.json()
-    } */}
-
-    const handleSubmit = (e) => {
+    }
+      
+    const updateUser = async(e) => {
         e.preventDefault()
         if(file){
-            fileChecksum(file).then((res) =>{
-                const checksum = res
-                console.log(checksum)
-                fetch('/presigned_url', {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        "file" : {
-                            "filename": file.name,
-                            "byte_size": file.size,
-                            "checksum": checksum,
-                            "content_type": "image/png",
-                            "metadata": {
-                                "message": "profile_image"
-                            }
-                        }
-                    })
-                }).then(function(res){
-                    return res.json()
-                }).then(function(data) {
-                    const url = data.direct_upload.url
-                    const headers = data.direct_upload.headers
-                    const blob_signed_id = data.blob_signed_id
-                    console.log(blob_signed_id)
-                    fetch(`/api/users/${user.id}`, {
-                        method: "PATCH",
-                        headers: {
-                            "Content_type": "image/png"
-                        },
-                        body: JSON.stringify({
-                            "id" : user.id,
-                            "username" : user.username,
-                            "png" : blob_signed_id
-                    })
-                    }).then(function(res){
-                        console.log(res)
-                        return res.json()
-                    }).then(function(data){
-                        console.log(data)
-                    })
-                })
-            })
+            const checksum = await fileChecksum(file)
+
+            const presignedFileParams = await createPresignedUrl(file, file.size, checksum)
+
+            const s3PutOptions = {
+                method: 'PUT',
+                headers: presignedFileParams.direct_upload.headers,
+                body: file,
+            }
             
+            let awsRes = await fetch(presignedFileParams.direct_upload.url, s3PutOptions)
+            if (awsRes.status !== 200) return awsRes
+
+            let userPatchOptions = {
+                method: 'PATCH',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    png: presignedFileParams.blob_signed_id,
+                })
+            }
+            let res = await fetch(`/api/users/${user.id}`, userPatchOptions)
+            if (res.status !== 200){ 
+                return res 
+            }else{
+                window.location.reload(false)
+            }
+            return await res.json()
         }
     }
-
-    {/*fetch(`/api/users/${user.id}`, {
-        method: "PATCH",
-        headers: {
-            "Content-type": "image/png"
-        },
-        body: JSON.stringify({
-            "id": user.id,
-            "username": user.username,
-            "png" : blob_signed_id
-    })
-    })*/}
 
     return(
         <div className='account-settings'>
@@ -173,10 +104,10 @@ const AccountSettings = () => {
                 <div onClick={() => navigate('/dashboard')}>{'<'} Back to dashboard</div>
             </div>
             <div className='account-div-right'>
-                <div><img alt='user' src={user.profile_image_url}></img></div>    
-                <form className='upi' onSubmit={handleSubmit}>
-                    <label htmlFor='profile-image'>Update profile image: </label>
-                    <input 
+                <div><img alt='user' src={user.profile_image_url ? `${user.profile_image_url}` : 'https://i.imgur.com/qbBOch9.png'}></img></div>    
+                <form className='upi' onSubmit={updateUser}>
+                    <input
+                        id='account-choose' 
                         type='file' 
                         name='profile-image'
                         onChange={(e) => setFile(e.target.files[0])}
--- client/src/features/user/User.js
+++ client/src/features/user/User.js
@@ -19,7 +19,7 @@ const User = () => {
     return(
         <div className='user' onClick={handleToggle}>
             <div className='user-details'>
-                <img alt='user avi' src={`${user.profile_image_url}`}></img>
+                <img alt='user avi' src={user.profile_image_url ? `${user.profile_image_url}` : 'https://i.imgur.com/qbBOch9.png'}></img>
                 <div className={ menuActive ? 'user-menu-arrow-up' : 'user-menu-arrow-down'}>
                     <AiOutlineMinus id='minus-1' /><AiOutlineMinus id={menuActive ? 'minus-2' : 'minus-2-open'} />
                 </div>
