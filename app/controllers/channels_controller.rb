b20c23a396b04ac9435069769b2fa05173e3c6b3
Don Odom <divdonis@gmail.com>
2022-02-22 11:44:48 -0800

    updated channel controller, custom method only rendered if name excat name

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -58,7 +58,7 @@ class ChannelsController < ApplicationController
     end
 
     def show_by_name
-        channels = Channel.by_name(params[:name])
+        channels = Channel.where("name like ?", "#{params[:name]}%") 
         filteredChannels = channels.reject{|x| @current_user.channels.include? x}
         if filteredChannels
             render json: filteredChannels
